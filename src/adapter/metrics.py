6b5bec4bf23846f5068991472c3d1f46728e778a
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-08-18 00:51:16 +0000

    feat: datadog adapter

--- src/adapter/metrics.py
+++ src/adapter/metrics.py
@@ -5,7 +5,7 @@ from src.application.ports.metrics import MetricsPort
 class DataDogAdapter(MetricsPort):
     def __init__(self, client):
         self.options = {"statsd_host": "35.226.251.154", "statsd_port": 8125}
-        datadog.initialize(**self.options)gi
+        datadog.initialize(**self.options)
 
     def increment(self, metric_name: str, tags: list, value: int = 1) -> None:
-        self.client.increment(metric_name, tags=tags, value=value)
+        datadog.statsd.increment(metric_name, tags=tags, value=value)
