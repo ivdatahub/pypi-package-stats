d497e405867ba1de61e185ab06b6e1e5373bb573
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-08-30 04:48:59 +0000

    feature: UseCase methods development

---
+++ src/application/__main__.py
@@ -0,0 +1,6 @@
+from application.use_cases.send_metrics_use_case import SendPypiStatsUseCase
+
+if __name__ == "__main__":
+    send_pypi_stats_use_case = SendPypiStatsUseCase()
+    df = send_pypi_stats_use_case.get_stats(package_name="currency-quote")
+    send_pypi_stats_use_case.send_stats(df=df)
--- src/application/use_cases/send_metrics_use_case.py
+++ src/application/use_cases/send_metrics_use_case.py
@@ -1,15 +1,34 @@
 from src.application.services.get_data_from_dw_service import GetDataFromDWService
-from src.application.services.get_secret_value_service import GetSecretValueService
 from src.application.services.send_metrics_service import SendMetricsService
 
 from src.adapter.bigquery_adapter import BigQueryAdapter
-from src.adapter.secret_manager_adapter import SecretManagerAdapter
 from src.adapter.datadog_adapter import DataDogAPIAdapter
+
+from pandas.core.frame import DataFrame
 
 
 class SendPypiStatsUseCase:
-    def __init__(self, metric_name: str, tags: list, value: int):
-        self.metrics_repository = DataDogAPIAdapter(metric_name, tags, value)
+    def __init__(self):
+        self.datawarehouse_adapter = BigQueryAdapter()
+        self.metrics_repository = DataDogAPIAdapter(metric_name="pypi")
 
-    def send(self):
-        SendMetricsService(MetricsRepository=self.metrics_repository).send()
+    def get_stats(self, package_name: str):
+        query = f"""
+            SELECT
+            DTTM, COUNTRY_CODE, PROJECT, PACKAGE_VERSION, INSTALLER_NAME, PYTHON_VERSION, TOTAL_DOWNLOADS
+            FROM `ivanildobarauna.DW.PYPI_PROJ`
+            WHERE PROJECT = {package_name}
+            AND PUSHED is null limit 1
+            """
+        return self.datawarehouse_adapter.get_data(query=query)
+    
+    def send_stats(self, df: DataFrame):
+        for index, row in df.iterrows():
+            tags = [
+                f"country_code:{row['COUNTRY_CODE']}",
+                f"project:{row['PROJECT']}",
+                f"package_version:{row['PACKAGE_VERSION']}",
+                f"installer_name:{row['INSTALLER_NAME']}",
+                f"python_version:{row['PYTHON_VERSION']}",
+            ]
+            self.metrics_repository.increment(tags=tags, value=row['TOTAL_DOWNLOADS'], timestamp=row['DTTM'])
