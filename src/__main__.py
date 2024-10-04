86f21207b5f3b4a462a5f74ab328bcc26d2153d7
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-10-04 01:32:53 -0300

    fix: added exception

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,38 @@
 from src.application.use_cases.send_metrics_use_case import SendPypiStatsUseCase
+from src.application.utils.logger_module import logger, log_extra_info, LogStatus
+
 
 if __name__ == "__main__":
-    send_pypi_stats_use_case = SendPypiStatsUseCase()
-    print("Getting stats from DW...")
+    try:
+        send_pypi_stats_use_case = SendPypiStatsUseCase()
+    except Exception as e:
+        logger.error(
+            "Error Instance of SendPypiStatsUseCase",
+            extra=log_extra_info(
+                status=LogStatus.ERROR,
+                msg=f"Error executing query: {str(e)}",
+            ),
+        )
+
     df, err = send_pypi_stats_use_case.get_stats()
-    print("Sending stats to DataDog...")
-    send_pypi_stats_use_case.send_stats(df=df)
-    print("Done!")
+    if err:
+        logger.error(
+            "Error Getting Stats from DW",
+            extra=log_extra_info(
+                status=LogStatus.ERROR,
+                msg=str(e),
+            ),
+        )
+        raise Exception(str(err))
+
+    try:
+        send_pypi_stats_use_case.send_stats(df=df)
+    except Exception as e:
+        logger.error(
+            "Error Sending Stats to DataDog",
+            extra=log_extra_info(
+                status=LogStatus.ERROR,
+                msg=str(e),
+            ),
+        )
+        raise Exception(str(e))
